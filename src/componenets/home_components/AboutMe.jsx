7d4f9bcec1458d7aca5dba492016abe1b4002e6a
Paul Lim <polim@Pauls-MacBook-Air.local>
2024-06-11 12:02:59 -0400

    updates to aboutme

--- src/componenets/home_components/AboutMe.jsx
+++ src/componenets/home_components/AboutMe.jsx
@@ -9,11 +9,14 @@ const AboutMe = () => {
       />
       <div className={styles.about_text}>
         <h1>About Me</h1>
-        <p>im something somethinethinethinethinethinething</p>
-        <p>what sets me apart</p>
-        <p>what sets me apart</p>
-        <p>what sets me apart</p>
-        <p>what sets me apart</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae tristique risus. Vestibulum quis mollis
+          velit, in aliquet velit. In at pretium enim. Donec a velit ac mi varius dictum. Curabitur placerat nulla et
+          tellus scelerisque, vel lacinia sem molestie. Morbi a nunc volutpat, rutrum arcu non, molestie urna. Nullam
+          vitae nunc vel odio semper bibendum interdum a nulla. Etiam sed magna vel lacus venenatis maximus. Maecenas
+          vehicula congue sem, sodales feugiat tellus. Phasellus fermentum volutpat nulla, eu gravida neque pulvinar
+          non.
+        </p>
         <button className={styles.button}>About Me</button>
       </div>
     </div>
